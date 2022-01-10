997f166bad60daccd0f6f2ec85cbc7a8722adeac
MarissZZZ <marisszuss@gmail.com>
2022-01-10 09:36:45 +0200

    Izmainas

--- script.js
+++ script.js
@@ -11,34 +11,39 @@ function nolasa() {
     const m3 = parseFloat(mala3.value); // ievadīto informāciju atgriež kā decimālu skaitli
 
     if (m1 > 0 && m2 > 0 && m3 > 0) {
-        console.log({m1, m2, m3});
-        return {m1, m2, m3}
+        console.log({ m1, m2, m3 });
+        return { m1, m2, m3 }
     } else {
         console.log(false);
-        return false;
+        return (false);
     }
 
     // perimetra aprēķināšana
 
-    function perimeter(m1, m2, m3) {
+    function perimetrs(m1, m2, m3) {
+
+        const m = nolasa();
+        m1 = m.m1; m2 = m.m2; m3 = m.m3;
+        console.log(m1, m2, m3);
+
         const p = m1 + m2 + m3;
         console.log(p);
-        return p;
+        return (p);
         // vajag atgriezt izrēķināto perimetru
     }
 
-    
-    function pusperimeter(m1, m2, m3) {
-        const pusp=p/2;
+
+    function pusperimetrs(m1, m2, m3) {
+        const pusp = p / 2;
         console.log(pusp);
-        return pusp;
+        return (pusp);
         // vajag atgriezt izrēķināto pusperimetru
     }
 
     function laukums(m1, m2, m3) {
         const pusp = perimeter(m1, m2, m3) / 2;
-        const laukums1= (math.sqrt(pusp*(pusp-m1)(pusp-m2)(pusp-m3)))
+        const laukums1 = (math.sqrt(pusp * (pusp - m1)(pusp - m2)(pusp - m3)))
         console.log(laukums1);
-        return laukums1;
+        return (laukums1);
     }
 }
